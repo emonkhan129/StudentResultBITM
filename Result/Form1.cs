1c300f8a91077ed39a9ea0020daa8f4ea9fa8690
emonkhan129 <emonkhan129@yahoo.com>
2016-10-06 23:52:24 +0600

    new project added

---
+++ Result/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Result
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            double physics = Convert.ToDouble(physicsTextBox.Text);
+            double chemistry = Convert.ToDouble(chemistryTextBox.Text);
+            double math = Convert.ToDouble(mathTextBox.Text);
+
+            Result aResult = new Result();
+            aResult.Chemistry = physics;
+            aResult.Physics = physics;
+            aResult.Math = math;
+
+            avarageTextBox.Text = aResult.GetAvarage().ToString();
+            gradeLetterTextBox.Text = aResult.GradeLetter();
+        }
+    }
+}
---
+++ Result/Result.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Result
+{
+    class Result
+    {
+        public double Physics { set; get; }
+        public double Chemistry { set; get; }
+        public double Math { set; get; }
+        private double avarage;
+
+        public double GetAvarage()
+        {
+            avarage = (Physics + Chemistry + Math) / 3;
+            return avarage;
+        }
+
+        public string GradeLetter()
+        {
+            string letter;
+
+           
+           
+           
+           
+             if (avarage >= 40 && avarage < 50)
+            {
+                letter = "D";
+            }
+                 else if (avarage >= 50 && avarage < 60)
+            {
+                letter = "C";
+            }
+                 else if (avarage >= 60 && avarage < 70)
+            {
+                letter = "B";
+            }
+                 else if (avarage >= 70 && avarage < 80)
+            {
+                letter = "A";
+            }
+            else  if (avarage >= 80)
+            {
+                letter = "A+";
+            }
+            else
+            {
+                letter = "F";
+            }
+
+            return letter;
+        }
+    }
+}
